000eec03bf4a017c5106cf2c63597cfb195f4198
krisu-g <grabowski.krzysztof@gmail.com>
2021-02-14 05:55:34 +0100

    Initial version of the package: - added reading from config.json - basic argparse support

---
+++ batchexec/__init__.py
@@ -0,0 +1,1 @@
+from .batch_executer import UniversalBatchExecuter
---
+++ main.py
@@ -0,0 +1,8 @@
+"""Execute app from config.json options with its arguments on all specified files in given input directory,\nand put the result into the output directory maintaining its structure."""
+import batchexec
+
+
+if __name__ == '__main__':
+    batch_executer = batchexec.UniversalBatchExecuter()
+    
+    batch_executer.run()
